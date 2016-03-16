3e98c34dfb07643988048bd44d55e8328fbb6154
Nick Koxlien <koxlienn@gmail.com>
2016-03-16 17:31:40 -0500

    Comparator Test Bench
    Finished comparator test bench

--- demo1/verilog/comparator_tb.v
+++ demo1/verilog/comparator_tb.v
@@ -1,7 +1,8 @@
 module comparator_tb();
 
  reg [15:0] A, B;
- reg clk, out;
+ reg clk;
+ wire out;
  
  initial begin
    clk = 0;
@@ -9,9 +10,19 @@ module comparator_tb();
    B = 16'h0000;
  end
  
- always @(#5 posedge clk) begin
- 
- 
+ always @(posedge clk) begin
+ 	if(((A > B) &&  !out) || ((A < B) && out))begin
+ 		$display("Somethings wrong\n");
+ 		$stop;	
+ 	end
+ 	
+ 	else if(A == 0) begin
+ 		$display("Success!\n");
+ 		$stop;
+ 	end
+ 	
+ 	A = A-1;
+ 	B = B + 1;
  end
  
  always #5 clk = ~clk;
