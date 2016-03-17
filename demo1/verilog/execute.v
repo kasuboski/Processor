4bfdbf13ee6e03cfd6876522206a9656083d85fe
Nick Koxlien <koxlienn@gmail.com>
2016-03-16 21:09:40 -0500

    Finished execute stage
    Execute stage is finished.  Just need an additional 2-bit BranchOp
    signal from ALU that will choose the appropriate zero equality to use
    for the branches (just use 00 if no branch occurring).  Also need to
    add invSrc1 and invSrc2 signals from control to tell ALU if either of
    the source signals to the ALU need to be inverted, and also a sub
    signal to let the ALU know if subtraction is occurring.

--- demo1/verilog/execute.v
+++ demo1/verilog/execute.v
@@ -1,20 +1,53 @@
-module execute(readdata1, readdata2, immediate, ALUOp, ALUSrc, invSrc1, invSrc2, sub, PC, jump, jumpReg, branch, nextPC, ALURes, err);
+module execute(readdata1, readdata2, immediate, BranchOP, ALUOp, ALUSrc, invSrc1, invSrc2, sub, PC, jump, jumpReg, branch, nextPC, ALURes, err);
 	
 	input [15:0] readdata1, readdata2, immediate, PC, nextPC;
 	input [3:0] ALUOp;
+	input [1:0] BranchOp;
 	input ALUSrc, jump, jumpReg, branch, invSrc1, invSrc2, sub;
 	
 	output [15:0] nextPC, ALURes;
 	output err;
 	
-	wire [15:0] src2;
-	wire sign, zero;
+	wire [15:0] src2, pcImmAddSum;
+	wire sign, zero, aluERR, jumpErr, pcImmAddOfl, LTZ, GEZ, NEZ;
+	
+	reg branchCondition;
 	
 	assign src2 = (ALUSrc) ? readdata2 : immediate;
 	assign sign = (ALUOp == 4'b1000) ? 1'b1 : 1'b0; 
+	assign jumpErr = pcImmAddOfl & ((branch & zero) | )
+	
+	assign GEZ = ~LTZ;
+	assign NEZ = ~zero;
+	assign LTZ = (ALURes[15]);
 	
 	//Directly linked the error signal to the overflow signal
-	alu ALU(.A(readdata1), .B(src2), .Cin(sub), .Op(ALUOp), .invA(invSrc1), .invB(invSrc2), .sign(sign), .Out(ALURes), .Ofl(err), .zero(zero));
+	alu ALU(.A(readdata1), .B(src2), .Cin(sub), .Op(ALUOp), .invA(invSrc1), .invB(invSrc2), .sign(sign), .Out(ALURes), .Ofl(aluErr), .zero(zero));
 	
-	adder pcImmAdd();
+	adder pcImmAdd(.A(PC), .B(immediate), .Cin(1'b0), .Overflow(pcImmAddOfl), .Cout(), .Sum(pcImmAddSum));
+	
+	assign nextPC = ((branchCondition & branch) | jump) ? pcImmAddSum : PC;
+	assign jumpErr = ((branchCondition & branch) | jump) & pcImmAddOfl;
+	assign err = aluErr | jumpErr;
+	
+	always @(*) begin
+		casex(BranchOp)
+			//In the case of no branch, just use 00 as branch opcode.
+			2'b00 : 
+				branchCondition = zero;
+			
+			2'b01 : 
+				branchCondition = NEZ;
+			
+			2'b10 :
+				branchCondition = LTZ;
+			
+			2'b11 : 
+				branchCondition = GEZ;
+				
+			default : 
+				branchCondition = 1'bx;	
+		endcase
+	end
+	
 endmodule
