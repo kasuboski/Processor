5e7b2ca64da8ce593dbef99d34a0dc848bcf3e33
Josh Kasuboski <kasuboski@wisc.edu>
2016-04-19 11:29:52 -0500

    Added a couple more output files to script

--- cache_direct/verification/parseOutput.rb
+++ cache_direct/verification/parseOutput.rb
@@ -1,17 +1,25 @@
 lines = []
+errors = []
 File.open(ARGV[0], "r").each_line do |line|
-	if(lineParts[0] == "#" && lineParts[1] == "LOG:" && lineParts[2] != "Done")
-		puts line
-		lines.push(lineParts)	
+	lineParts = line.split(" ")
+	if lineParts[0] == "#" && lineParts[1] == "LOG:" && lineParts[2] != "Done"
+		lines.push(lineParts)
+	elsif lineParts[0] == "#" && lineParts[1] == "ERROR"
+		errors.push("Line: " + lines.length.to_s + " " + lines.last.join(" ")) 		
 	end
 end
 
-file = File.open("test.addr", 'w')
+addr_file = File.open("test.addr", 'w')
+log_file = File.open("test.log", 'w')
+error_file = File.open("test.error", 'w')
 
 lines.each do |line|
 	write = (line[8] == "Wr") ? 1 : 0
 	read = (line[8] == "Rd") ? 1 : 0
 	addr = line[10].hex.to_s
 	value = line[12].hex.to_s
-	file.write(write.to_s + " " + read.to_s + " " + addr + " " + value + "\n")
+	addr_file.write(write.to_s + " " + read.to_s + " " + addr + " " + value + "\n")
+	log_file.write(line.join(" ") + "\n")
 end
+
+error_file.write(errors.join("\n"))
