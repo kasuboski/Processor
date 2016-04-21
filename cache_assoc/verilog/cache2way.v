6701b7e0b1c6e9ad76ef87252c433c275bcba1f4
Nick Koxlien <nkoxlien@wisc.edu>
2016-04-20 22:53:25 -0500

    Works

--- cache_assoc/verilog/cache2way.v
+++ cache_assoc/verilog/cache2way.v
@@ -28,7 +28,8 @@ module cache2way(
 	wire write0, write1;
 
 	wire victimway;
-	reg selectedCache;
+	wire selectedCache, selectedCacheIn;
+	reg selectedCacheSel;
 
         /* data_mem = 1, inst_mem = 0 *
          * needed for cache parameter */
@@ -74,36 +75,43 @@ module cache2way(
                           .write                (write1),
                           .valid_in             (valid_in));
 
-	assign hit = selectedCache ? hit1: hit0;
-	assign valid = selectedCache ? valid1 : valid0;
-	assign dirty = selectedCache ? dirty1 : dirty0;
-	assign data_out = selectedCache ? data_out1 : data_out0;
-	assign tag_out = selectedCache ? tag_out1 : tag_out0;
+	assign hit = ((hit1 & valid1) | (hit0 & valid0));
+	assign valid = valid1 | valid0;
+	assign dirty = hit ? (hit1 & valid1 ? dirty1 : dirty0) :
+			 (comp ? (selectedCacheSel ? dirty1 : dirty0) : (selectedCache ? dirty1 : dirty0));
+	assign data_out = hit ? (hit1 & valid1 ? data_out1 : data_out0) :
+			 (comp ? (selectedCacheSel ? data_out1 : data_out0) : (selectedCache ? data_out1 : data_out0));
+
+	assign tag_out = hit ? (hit1 & valid1 ? tag_out1 : tag_out0) :
+			 (comp ? (selectedCacheSel ? tag_out1 : tag_out0) : (selectedCache ? tag_out1 : tag_out0));
 	assign err = err0 | err1; 
 
 	assign victimwayIn = invert_victimway ? ~victimway : victimway;
 	dff victimway0(.q(victimway), .d(victimwayIn), .clk(clk), .rst(rst));
 	
-	assign write0 = selectedCache ? 1'b0 : write;
-	assign write1 = selectedCache ? write : 1'b0;
+	assign write0 = hit0 & valid0 ? write : comp ? 1'b0 :  selectedCache ? 1'b0 : write;
+	assign write1 = hit1 & valid1 ? write : comp ? 1'b0 :  selectedCache ? write : 1'b0;
+
+	assign selectedCacheIn = comp ? selectedCacheSel : selectedCache;
+	dff selectedCache0(.q(selectedCache), .d(selectedCacheIn), .clk(clk), .rst(rst));
 
 	always @(hit0, hit1, valid0, valid1, victimway) begin
 		casex({hit0, hit1, valid0, valid1})
 		//cache 0 has a hit
-		4'b1_0_x_x: selectedCache = 1'b0;
+		4'b1_0_x_x: selectedCacheSel = 1'b0;
 		//cache 1 has a hit
-		4'b0_1_x_x: selectedCache = 1'b1;
+		4'b0_1_x_x: selectedCacheSel = 1'b1;
 		//in case of miss
 		//one is valid
 		//cache 0 is valid
-		4'b0_0_1_0: selectedCache = 1'b1;
+		4'b0_0_1_0: selectedCacheSel = 1'b1;
 		//cache 1 is valid
-		4'b0_0_0_1: selectedCache = 1'b0;
+		4'b0_0_0_1: selectedCacheSel = 1'b0;
 		//neither is valid
-		4'b0_0_0_0: selectedCache = 1'b0;
+		4'b0_0_0_0: selectedCacheSel = 1'b0;
 		//both are valid
-		4'b0_0_1_1: selectedCache = victimway;
-		default: selectedCache = 1'b0;
+		4'b0_0_1_1: selectedCacheSel = victimway;
+		default: selectedCacheSel = 1'b0;
 		endcase
 	end
 
